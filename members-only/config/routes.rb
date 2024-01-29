ba298e4a86bc844fcd2de48f814151be887cfb65
Andres Felipe -Main <andrefe91@gmail.com>
2024-01-29 00:27:56 -0500

    New article view and some modifications to the Index view

--- members-only/config/routes.rb
+++ members-only/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root "posts#index"
 
   get "/posts", to: "posts#index"
+  get "/posts/new", to: "posts#new"
   get "/posts/:id", to: "posts#show"
 
   resource :users
